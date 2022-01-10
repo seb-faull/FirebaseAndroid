e1973c0e14265f319fb298541dc918e378c80ea1
bxxstian <seb_faull@hotmail.co.uk>
2022-01-10 18:16:35 +0000

    Add Register logic to MainActivity.

--- app/src/main/java/com/example/firebaseauthapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.firebaseauthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
+    private final static int RSRCS_REGISTER = R.id.register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(RSRCS_REGISTER);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case RSRCS_REGISTER:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
     }
 }
