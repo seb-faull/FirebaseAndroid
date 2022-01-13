ada43575035f3c4088edd4f8bf9294049f3f97c4
bxxstian <seb_faull@hotmail.co.uk>
2022-01-13 16:57:22 +0000

    Add validation on register user screen.

--- app/src/main/java/com/example/firebaseauthapp/RegisterUser.java
+++ app/src/main/java/com/example/firebaseauthapp/RegisterUser.java
@@ -1,13 +1,93 @@
 package com.example.firebaseauthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class RegisterUser extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView banner, registerUser;
+    private EditText editTextFullName, editTextAge, editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.fullName);
+        editTextAge = (EditText) findViewById(R.id.age);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.registerUser:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+        String age = editTextAge.getText().toString().trim();
+
+        if (fullName.isEmpty()) {
+            editTextFullName.setError("Please enter your full name.");
+            editTextFullName.requestFocus();
+        }
+
+        if (age.isEmpty()) {
+            editTextAge.setError("Please enter your age.");
+            editTextAge.requestFocus();
+        }
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Please enter your email address.");
+            editTextEmail.requestFocus();
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide a valid email address.");
+            editTextEmail.requestFocus();
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Please enter your password.");
+            editTextPassword.requestFocus();
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum password length should be 6 characters.");
+            editTextPassword.requestFocus();
+        }
     }
 }
