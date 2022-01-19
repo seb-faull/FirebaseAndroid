a609d5fd9371ab313c30ee789a52de283b3e11ac
bxxstian <seb_faull@hotmail.co.uk>
2022-01-19 20:47:47 +0000

    Add logout func.

--- app/src/main/java/com/example/firebaseauthapp/ProfileActivity.java
+++ app/src/main/java/com/example/firebaseauthapp/ProfileActivity.java
@@ -2,13 +2,26 @@ package com.example.firebaseauthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logout = (Button) findViewById(R.id.signOut);
+        logout.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+        });
     }
 }
